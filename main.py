3bfefe2e529cfdd0db248f644140256d759a845d
Spillz <billygcasey@hotmail.com>
2018-04-13 08:48:55 -0500

    Main parts working. Just need tweeking of html

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:beproductive@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 
 class Blog(db.Model):#needed that all to creat database and ability to enter 
@@ -14,32 +15,71 @@ class Blog(db.Model):#needed that all to creat database and ability to enter
     blg_title = db.Column(db.String(120))
     blg_body = db.Column(db.String(120)) #i dont know the max
     ##Part of blogz   keep off until ready
-    #owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, blg_title, blg_body):
+    def __init__(self, blg_title, blg_body, owner):
         self.blg_title = blg_title
         self.blg_body = blg_body
-        ##Part of blogz  keep off until ready
-        #self.owner = owner
+        self.owner = owner
 
+class User(db.Model):
 
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    tasks = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
 
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
     
     blog = Blog.query.all()
-    #bloglink_id = request.form['bloglink']
-    #bloglink_id = request.form['bloglink']
-    #print(bloglink_id)
-    #bloglink = Blog.query.get(bloglink_id)
-    #user = Blog.query.get(id).first()
+    id=request.args.get('id')
+    if id:
+        id=(int(id))
 
+        sngl_post=Blog.query.filter(Blog.id==id).first()
+        blg_title = sngl_post.blg_title
+        blg_body = sngl_post.blg_body
+        print(blg_title, blg_body)
+        return render_template('blog.html', title=blg_title, blg_body=blg_body)
 
     return render_template('blog.html',title="Build a Blog", blog=blog)
+
+@app.route('/singleuser', methods=['POST', 'GET'])
+def singleuser():
+    
+    blog = Blog.query.all()
+    id=request.args.get('id')
+    owner = User.query.filter_by(email=session['email']).first()
+    blog = Blog.query.filter_by(owner=owner).all()
+    print(owner, owner )
+    if id:
+        id=(int(id))
+
+        sngl_post=Blog.query.filter(Blog.id==id).first()
+        blg_title = sngl_post.blg_title
+        blg_body = sngl_post.blg_body
+        print(blg_title, blg_body)
+        return render_template('singleuser.html', title=blg_title, blg_body=blg_body)
+
+    return render_template('singleuser.html',title="My Blogz", blog=blog)
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
+    owner = User.query.filter_by(email=session['email']).first()
+
+
 
     if request.method == 'POST':
         blg_title = request.form['blg_title']
@@ -49,40 +89,19 @@ def newpost():
             return render_template('newpost.html', blg_fail=blg_fail, blg_title=blg_title, blg_body=blg_body) 
         #Part of Blogz. Delete this next line when done with it
         #owner_id = ("?") #may als need to remove this v
-        new_entry = Blog(blg_title, blg_body)#, owner_id)
+        new_entry = Blog(blg_title, blg_body, owner)
         db.session.add(new_entry)
         db.session.commit()
         return render_template('blog_entry.html', blog=new_entry)
 
     return render_template('newpost.html')
 
-#####THis isnt working yet V
-@app.route('/blog_entry', methods=['POST', 'GET'])
-def blog_entry(self, id):
-    #blog = Blog.get_by_id(int(id))
-    #tmplt = jinja.get_template('blog_entry.html')
-    #response = tmplt.render(blog=blog)
 
-    #blog = Blog.query.all()
-    #blog_id = request.form['{{blog.id}}']
-    #blog = Blog.query.filter_by(blog_id=blog_id).all()
-  
-    return render_template('blog_entry.html' + blog.id, blog=blog)
 
 ##############################################
 ####################### For Blogz
 ###User stuff
-class User(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    #check out this part, refering to Blog Keep off and edit when ready
-    #tasks = db.relationship('Blog', backref='owner')
-
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -92,17 +111,18 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and user.password == password:
             session['email'] = email
-            flash("Logged in")
-            return redirect('/')
-        else:
-            flash('User password incorrect, or user does not exist', 'error')
+            #flash("Logged in")
+            #return redirect('/blog')
+            return redirect("/blog")#this works
+        #else:
+            #flash('User password incorrect, or user does not exist', 'error')
 
     return render_template('login.html')
 
 ### End User stuff
-##Register
+##signup
 @app.route('/signup', methods=['POST', 'GET'])
-def register():
+def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -115,8 +135,8 @@ def register():
             new_user = User(email, password)
             db.session.add(new_user)
             db.session.commit()
-            #session['email'] = email
-            return redirect('/')
+            session['email'] = email
+            return redirect('/blog')
         else:
             # TODO - user better response messaging
             blg_fail = ("Email has already been registered")
@@ -124,7 +144,12 @@ def register():
 
 
     return render_template('signup.html')
-##end register
+##end signup
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/blog')
 
 if __name__ == '__main__':
     app.run()
